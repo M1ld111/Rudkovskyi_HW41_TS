9d37c68ac82d4ede3003718154500787b53296e7
Mild <maxxfulll@gmail.com>
2023-04-29 15:39:02 +0300

    second

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,81 @@
 import { useTodoContext } from "../providers/TodoProvider";
-import { List, ListItem, Button, Box } from '@mui/material';
-
+import { List, ListItem, Button, Box, TextField } from "@mui/material";
+import { useState } from "react";
 
 const TodoList = () => {
-    const { todoList, removeTodo } = useTodoContext();
+  const { todoList, removeTodo, startEditTodo, endEditTodo } = useTodoContext();
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editedTodo, setEditedTodo] = useState<string>("");
 
-    return ( 
-        <List>
-            {todoList.map((todo, index) => (
-                <ListItem key={index}>
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        gap: '20px'
-                    }}>
-                        {todo}
-                        <Button variant="outlined" onClick={() => removeTodo(index)}>Delete Todo</Button>
-                    </Box>
-                </ListItem>
-            ))}
-        </List> 
-        );
-};  
+  const handleEditClick = (index: number) => {
+    setEditingIndex(index);
+    startEditTodo(index);
+    setEditedTodo(todoList[index]);
+  };
+
+  const handleEditChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTodo(event.target.value);
+  };
+
+  const handleEditSave = (index: number) => {
+    endEditTodo(editedTodo);
+    setEditingIndex(null);
+    setEditedTodo("");
+  };
+
+  const handleEditCancel = () => {
+    setEditingIndex(null);
+    setEditedTodo("");
+  };
+
+  return (
+    <List>
+      {todoList.map((todo, index) => (
+        <ListItem key={index}>
+          {editingIndex === index ? (
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "20px",
+              }}
+            >
+              <TextField
+                value={editedTodo}
+                onChange={handleEditChange}
+                fullWidth
+              />
+              <Button variant="contained" onClick={() => handleEditSave(index)}>
+                Save
+              </Button>
+              <Button variant="outlined" onClick={handleEditCancel}>
+                Cancel
+              </Button>
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "20px",
+              }}
+            >
+              {todo}
+              <Button
+                variant="outlined"
+                onClick={() => handleEditClick(index)}
+              >
+                Edit
+              </Button>
+              <Button variant="outlined" onClick={() => removeTodo(index)}>
+                Delete
+              </Button>
+            </Box>
+          )}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 export default TodoList;
--- src/providers/TodoProvider.tsx
+++ src/providers/TodoProvider.tsx
@@ -5,6 +5,8 @@ interface TodoContextType {
   numberOfTodos: () => number;
   addTodo: (newTodoItem: string) => void;
   removeTodo: (todoIndex: number) => void;
+  startEditTodo: (todoIndex: number) => void;
+  endEditTodo: (updatedTodo: string) => void;
 }
 
 const TodoContext = createContext<TodoContextType>({
@@ -12,33 +14,54 @@ const TodoContext = createContext<TodoContextType>({
   numberOfTodos: () => 0,
   addTodo: (newTodoItem: string) => {},
   removeTodo: (todoIndex: number) => {},
+  startEditTodo: (todoIndex: number) => {},
+  endEditTodo: (updatedTodo: string) => {},
 });
 
 const initialTodoListState = ["First todo"];
 
 const TodoProvider = ({ children }: { children: ReactNode }) => {
   const [todoList, setTodoList] = useState(initialTodoListState);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
 
-  const numberOfTodos = () => todoList.length
+  const numberOfTodos = () => todoList.length;
 
   const addTodo = (newTodoItem: string) => {
-    setTodoList([...todoList, newTodoItem])
-  }
+    setTodoList([...todoList, newTodoItem]);
+  };
 
   const removeTodo = (todoIndex: number): void => {
-    const newList =  todoList.filter((_, index) => index !== todoIndex)
-    setTodoList(newList)
-  }
+    const newList = todoList.filter((_, index) => index !== todoIndex);
+    setTodoList(newList);
+  };
+
+  const startEditTodo = (index: number) => {
+    setEditingIndex(index);
+  };
+
+  const endEditTodo = (updatedTodo: string) => {
+    if (editingIndex === null) {
+      return;
+    }
+    const newTodoList = [...todoList];
+    newTodoList[editingIndex] = updatedTodo;
+    setTodoList(newTodoList);
+    setEditingIndex(null);
+  };
 
   const contextValue = {
     todoList,
     numberOfTodos,
     addTodo,
-    removeTodo
+    removeTodo,
+    startEditTodo,
+    endEditTodo,
   };
 
   return (
-    <TodoContext.Provider value={contextValue}>{children}</TodoContext.Provider>
+    <TodoContext.Provider value={contextValue}>
+      {children}
+    </TodoContext.Provider>
   );
 };
 
